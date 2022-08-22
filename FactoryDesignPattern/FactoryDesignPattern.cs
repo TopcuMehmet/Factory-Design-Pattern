aab254a87818c9f15e94a3bf02e19cc185331b0c
TopcuMehmet <111745319+TopcuMehmet@users.noreply.github.com>
2022-08-22 14:08:53 +0300

    Add files via upload

---
+++ FactoryDesignPattern/FactoryDesignPattern.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace FactoryDesignPattern
+{
+    public interface IShape
+    {
+        void Draw();
+    }
+
+    public class Rectangle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing Rectangle");
+        }
+    }
+
+    public class Square : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing Square");
+        }
+    }
+
+    public class Circle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing Circle");
+        }
+    }
+
+    class ShapeFactory
+    {
+        public IShape GetShape(String shapeType)
+        {
+            if (shapeType == null)
+            {
+                return null;
+            }
+
+            if (shapeType.Equals("CIRCLE"))
+            {
+                return new Circle();
+            }
+            else if (shapeType.Equals("RECTANGLE"))
+            {
+                return new Rectangle();
+            }
+            else if (shapeType.Equals("SQUARE"))
+            {
+                return new Square();
+            }
+            return null;
+        }
+    }
+
+
+
+}
---
+++ FactoryDesignPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FactoryDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShapeFactory shapeFactory = new ShapeFactory();
+            IShape shape = shapeFactory.GetShape("RECTANGLE");
+            shape.Draw();
+            IShape shape2 = shapeFactory.GetShape("SQUARE");
+            shape2.Draw();
+            IShape shape3 = shapeFactory.GetShape("CIRCLE");
+            shape3.Draw();
+            Console.ReadLine();
+        }
+    }
+}
